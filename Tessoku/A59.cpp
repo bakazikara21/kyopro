6f873929469f9647e5f4c9d78a26275951b9fb68
bakazikara21 <236205330+bakazikara21@users.noreply.github.com>
2025-12-13 11:27:07 +0900

    セグメント木やダブリングの問題を解きました

--- Tessoku/A59.cpp
+++ Tessoku/A59.cpp
@@ -1,7 +1,58 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
+// セグメント木RSQ
+void query1(int pos, int x, vector<int> &seg){
+    int dif = x-seg[pos];
+    seg[pos] = x;
+    while(pos > 1){
+        // 上層部の最大値区間を更新する
+        pos /= 2;
+        // seg[pos]が更新されなかったらbreakでもよいと思う
+        seg[pos] += dif;
+    }
+}
 
+int query2(int left, int right, vector<int> &seg, int pos, int first, int last){
+    // [left,right)に注意する
+    if(right <= first or left >= last) return 0;
+    if(left <= first and last <= right) return seg[pos];
+
+    int mid = (first+last)/2;
+    int AnswerL = query2(left,right,seg,pos*2,first,mid);
+    int AnswerR = query2(left,right,seg,pos*2 + 1,mid,last);
+    return AnswerL+AnswerR;
+}
 int main(){
+    int N,Q;
+    cin >> N >> Q;
 
+    int two = 1;
+    while(1){
+        if(two/2 < N and N <= two){
+            N = two;
+            break;
+        }
+        two *= 2;
+        if(two > 4*N) break;
+    }
+    vector<int> seg(2*N,0);
+    for(int q = 0; q < Q; q++){
+        int num;
+        cin >> num;
+
+        if(num == 1){
+            int pos,x;
+            cin >> pos >> x;
+
+            pos = pos+N-1;
+            query1(pos,x,seg);
+        }
+        if(num == 2){
+            int l,r;
+            cin >> l >> r;
+
+            cout << query2(l,r,seg,1, 1, N+1) << endl;
+        }
+    }
 }
--- Tessoku/A60.cpp
+++ Tessoku/A60.cpp
@@ -3,5 +3,30 @@ using namespace std;
 using ll = long long;
 
 int main(){
+    int N;
+    cin >> N;
 
+    vector<int> A(N),dp(N);
+    for(int i = 0; i < N; i++) cin >> A[i];
+
+    dp[0] = -1;
+    for(int i = 1; i < N; i++){
+        if(A[i-1] > A[i]){
+            dp[i] = i-1;
+        }else if(dp[i-1] == -1){
+            dp[i] = -1;
+        }else{
+            int x = dp[i-1];
+            while(x != -1 and A[x] <= A[i]){
+                x = dp[x];
+            }
+            if(x == -1) dp[i] = -1;
+            else dp[i] = x;
+        }
+    }
+    for(int i = 0; i < N; i++){
+        if(dp[i] == -1) cout << dp[i] << " ";
+        else cout << dp[i]+1 << " ";
+    }
+    cout << endl;
 }

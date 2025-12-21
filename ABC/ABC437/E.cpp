a80643e6df0e37fcfecd0494812d5667610f0b7d
bakazikara21 <236205330+bakazikara21@users.noreply.github.com>
2025-12-21 12:54:20 +0900

    ABC437のE.cppでTrie木を実装しました。

--- ABC/ABC437/E.cpp
+++ ABC/ABC437/E.cpp
@@ -1,32 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-
+// Trie木を使ってDFSして解く
+// 末尾に追加する整数を辺の重みとして実装する
 int main(){
     int N;
     cin >> N;
-    vector<vector<int>> A(N+1);
-    for(int i = 0; i < N; i++){
+    vector<set<int>> id(N+1); // 頂点iに含まれる添え字
+
+    vector<set<pair<int,int>>> graph(N+1); // 頂点iに{重み,移動先の頂点番号}を記憶させる
+
+    vector<int> vertex(N+1);    // Aiが属する頂点の番号
+    map<pair<int,int>,int> mp;  // {xが所属する頂点,重みy}のペアに対して頂点番号を対応させる
+    for(int i = 1; i <= N; i++){
         int x,y;
         cin >> x >> y;
-        // A_{i+1}はA_{x}の末尾にyを加えた
-        A[i+1].push_back(-x);
-        A[i+1].push_back(y);
-    }
-    vector<pair<vector<int>,int>> vec(N);
-    for(int i = 0; i < N; i++){
-        vec[i] = {A[i+1],i+1};
-    }
-    sort(vec.begin(),vec.end());
-    for(int i = 0; i < N; i++){
-        for(auto v : vec[i].first){
-            cout << v << " ,";
+        // {x,y}をmap,setで持っておいて記憶しておく
+        if(mp.count({vertex[x],y})){
+            int ver = mp[{vertex[x],y}];    // 既存の頂点verへ向かうx,y
+            id[ver].insert(i);
+            vertex[i] = ver;  // Aiが属する頂点の番号はver
+        }else{
+            mp[{vertex[x],y}] = i;  // {xが所属する頂点,辺の重みy}のペアに対して頂点番号iを割り振る
+            vertex[i] = i; // Aiが属する頂点の番号はi
+            graph[vertex[x]].insert({y,i});  // xが所属する頂点番号に{重みy,移動先i}を追加する
+            id[i].insert(i);
         }
-        cout << " i+1 = " << vec[i].second << endl;
     }
-    int cnt = 0;
-    for(int i = 0; i < N; i++){
-        int num = vec[i].first.at(0);
-        if(num < 0) cnt++;
+    // 重みは小さい順になっている(setの性質)
+
+    vector<int> P;  // DFSしながら答えをpush_back()していく
+    vector<bool> used(N+1,false);
+    auto dfs = [&](auto dfs, int v)->void{
+        for(auto [w,nv] : graph[v]){
+            if(used[nv] == true) continue;
+
+            for(auto num : id[nv]){
+                P.push_back(num);
+            }
+            used[nv] = true;
+            dfs(dfs,nv);
+            used[nv] = false;
+        }   
+        used[v] = false;
+        return;
+    };
+    used[0] = true;
+    dfs(dfs,0);
+    for(int p : P){
+        cout << p << " ";
     }
+    cout << endl;
 }

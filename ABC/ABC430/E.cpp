3c02bb273b6fbef916788c86345ef36ef0deeb61
bakazikara21 <236205330+bakazikara21@users.noreply.github.com>
2026-02-28 23:35:26 +0900

    ABC430Eを解こうとしたが寝る時間なのでやめた。

---
+++ ABC/ABC430/E.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const ll INF = (ll)2e18;
+const int inf = (int)1e9;
+const ll mod = 2147483647;
+
+int main(){
+    int T; cin >> T;
+    for(int t = 0; t < T; t++){
+        string A,B; cin >> A >> B;
+        int N = (int)A.size();
+
+        ll tenA = 0;    // Aを10進法になおすと無理
+    }
+}

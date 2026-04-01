e7a58aef145cd1c8bbf12e4ae2b165b407a4881d
bakazikara21 <236205330+bakazikara21@users.noreply.github.com>
2026-04-01 15:04:12 +0900

    遅延評価セグメント木をリファクタリングしました。

--- DataStructure/LazySegmentTree.cpp
+++ DataStructure/LazySegmentTree.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
+
 /*
     区間加算 add(int l, int r, ll x):
     例えば、N=8で区間[1,5]に+3するとき
@@ -9,12 +10,12 @@ using ll = long long;
     [5,5]に対応するlazyに+3
     をして終了する。
 
-    配列の値にアクセスするときに必要なとこだけ
-    lazyの値を下の子に伝播させる。
+    クエリ処理の途中で通過する配列全てで
+    lazyの値を下の子に伝播させ、値を更新する。
 */
 class LazySegmentTree {
     private:
-    vector<ll> tree;    // 遅延なしのセグメント木の配列
+    vector<ll> node;    // 遅延なしのセグメント木の配列
     vector<ll> lazy;    // アクセスしたときのみ更新するための値を保存しておく配列
     int n;
 
@@ -22,8 +23,8 @@ class LazySegmentTree {
     void propagate(int pos, int left, int right) {
         if (lazy[pos] == 0) return; // 遅延がなければ何もしない
 
-        // 自身の tree に遅延分を反映（1要素あたりの値 × 区間の長さ）
-        tree[pos] += lazy[pos] * (right - left);
+        // 自身の node に遅延分を反映（1要素あたりの値 × 区間の長さ）
+        node[pos] += lazy[pos] * (right - left);
 
         // 葉ノードでなければ子に伝播
         if (right - left > 1) {
@@ -36,25 +37,29 @@ class LazySegmentTree {
     }
 
     public:
-    LazySegmentTree(int N):tree(2*N,0),lazy(2*N,0){
+    LazySegmentTree(int N):node(2*N,0),lazy(2*N,0){
         n = N;
     }
 
-    // tree[pos]の値をxに初期化する
-    void initialize(int pos, ll x){
-        pos += n-1;
-        tree[pos] = x;
-        while(pos > 1){
-            pos /= 2;
-            tree[pos] = tree[pos*2]+tree[pos*2+1];
+    // node[pos]の値を初期化する
+    void initialize(vector<ll> A){
+        int N = (int)A.size();
+        for(int i = 0; i < N; i++){
+            node[i+n] = A[i];
+        }
+        for(int pos = n-1; pos >= 1; pos--){
+            node[pos] = node[pos*2]+node[pos*2+1];
         }
     }
 
     // 区間[l,r)に+xを加算する
-    void add(int l, int r, ll x, int pos, int left, int right){
-        // まず、区間[l,r)を上手に分割する
-        // 次に、lazyに値を入れて即座に伝播させる
-        
+    void add(int l, int r, ll x, int pos = 1, int left = 1, int right = -1){
+        /*
+            まず、区間[l,r)を高々log(n)個に分割する
+            その間に通過するノードは全てlazyを伝播させる
+            さらに、通過するノードの更新もO(1)で行う
+        */
+        if(right < 0) right = n+1;
         // 遅延伝播を逐次行う
         propagate(pos,left,right);
 
@@ -70,17 +75,18 @@ class LazySegmentTree {
         add(l,r,x,pos*2  ,left,mid);
         add(l,r,x,pos*2+1,mid ,right);
 
-        // 子の計算が終わった後、自身の tree を再計算して更新する
-        tree[pos] = tree[pos * 2] + tree[pos * 2 + 1];
+        // 子の計算が終わった後、自身の node を再計算して更新する
+        node[pos] = node[pos * 2] + node[pos * 2 + 1];
     }
 
     // 区間[l,r)の値の合計を返す
-    ll getRangeSum(int l, int r, int pos, int left, int right){
+    ll getRangeSum(int l, int r, int pos = 1, int left = 1, int right = -1){
+        if(right < 0) right = n+1;
         // posでの遅延伝播操作
         propagate(pos,left,right);
 
         if(right <= l or r <= left) return 0LL; // 範囲外
-        if(l <= left and right <= r) return tree[pos];
+        if(l <= left and right <= r) return node[pos];
 
         int mid = (right+left)/2;
         ll sumL = getRangeSum(l,r,pos*2,  left,mid);

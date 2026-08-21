538572408f022074fbabfd35bf06001567871303
bakazikara21 <236205330+bakazikara21@users.noreply.github.com>
2026-08-21 22:44:19 +0900

    焼きなまし法を学んだ。単に2-optで悪化するときに、低い確率で悪化させるだけ

--- Tessoku/A46.cpp
+++ Tessoku/A46.cpp
@@ -6,6 +6,10 @@ int randint(int a, int b){
     // a以上、b以下のランダムな整数を返す関数
     return a + rand() % (b-a+1);
 }
+long double randdouble(){
+    // 0以上1以下のランダムな実数を返す関数
+    return 1.0 * rand()/ RAND_MAX;
+}
 int main(){
     int N; cin >> N;
     vector<long double> X(N),Y(N);
@@ -44,9 +48,9 @@ int main(){
         --b;
         return hypotl(X[a] - X[b], Y[a] - Y[b]);
     };
-
-    for(int i = 0; i < 4e6; i++){
-        // 4e6回、ランダム2-optを行う
+    int imax = 1e6;
+    for(int i = 1; i <= imax; i++){
+        // imax回、ランダム2-optを行う
         int left = 0, right = 0;
         while(left == right){
             left = randint(1,N-1);
@@ -62,8 +66,14 @@ int main(){
         long double after =
             dist(path[left - 1], path[right]) +
             dist(path[left], path[right + 1]);
-    
-        if (after < before) {
+        
+        // 温度関数T: 時間がたつにつれ、Tは小さくなるべきらしい
+        long double T = 30.0 - (28.0*i/imax);
+
+        // 採用確率
+        long double proba = exp(min((long double)0.0, (before-after)/T));
+
+        if (randdouble() < proba) {
             reverse(path.begin() + left, path.begin() + right + 1);
         }
     }

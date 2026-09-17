7f81ee363e401a8485a4594de77c320b030be23c
bakazikara21 <236205330+bakazikara21@users.noreply.github.com>
2026-09-17 19:50:17 +0900

    ABC309のE問題は茶diffレベルです。

--- ABC/ABC300-399/ABC309/D.cpp
+++ ABC/ABC300-399/ABC309/D.cpp
@@ -5,5 +5,37 @@ const ll INF = (ll)2e18;
 const int inf = (int)1e9;
 
 int main(){
-    
+    int N1,N2,M; cin >> N1 >> N2 >> M;
+    vector<vector<int>> graph(N1+N2);
+    for(int i = 0; i < M; i++){
+        int u,v; cin >> u >> v; u--; v--;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+    int mx1 = 0, mx2 = 0;
+    queue<int> que;
+    que.push(0);
+    vector<int> dist1(N1,inf),dist2(N1+N2,inf);
+    dist1[0] = 0;
+    while(!que.empty()){
+        int v = que.front(); que.pop();
+        for(int nv : graph[v]){
+            if(dist1[nv] < inf) continue;
+            dist1[nv] = dist1[v]+1;
+            mx1 = max(mx1,dist1[nv]);
+            que.push(nv);
+        }
+    }
+    que.push(N1+N2-1);
+    dist2[N1+N2-1] = 0;
+    while(!que.empty()){
+        int v = que.front(); que.pop();
+        for(int nv : graph[v]){
+            if(dist2[nv] < inf) continue;
+            dist2[nv] = dist2[v]+1;
+            mx2 = max(mx2,dist2[nv]);
+            que.push(nv);
+        }
+    }
+    cout << mx1 + mx2 + 1 << endl;
 }
--- ABC/ABC300-399/ABC309/E.cpp
+++ ABC/ABC300-399/ABC309/E.cpp
@@ -5,5 +5,30 @@ const ll INF = (ll)2e18;
 const int inf = (int)1e9;
 
 int main(){
-    
+    int N,M; cin >> N >> M;
+    vector<int> p(N);
+    vector<vector<int>> graph(N);
+    for(int i = 1; i < N; i++){
+        cin >> p[i]; p[i]--;
+        graph[p[i]].push_back(i);
+    }
+    vector<int> val(N,-1);
+    for(int i = 0; i < M; i++){
+        int x,y; cin >> x >> y;
+        x--;
+        val[x] = max(val[x],y);
+    }
+    int ans = 0;
+    queue<int> que;
+    que.push(0);
+    while(!que.empty()){
+        int v = que.front(); que.pop();
+        int value = val[v];
+        if(value >= 0) ans++;
+        for(int nv : graph[v]){
+            val[nv] = max(val[nv],value-1);
+            que.push(nv);
+        }
+    }
+    cout << ans << endl;
 }
